e06faac81f7f07b04496243d907ef5897b107b97
Simon Brown <simon.j.brown@outlook.com>
2016-10-28 15:14:03 +1100

    First commit

---
+++ Provision.ps1
@@ -0,0 +1,38 @@
+﻿$subscriptionName = (Get-AzureSubscription | where {$_.IsCurrent -eq "True"}).SubscriptionName
+$clusterName = "TestCluster"
+$location = "<MicrosoftDataCenter>"
+$clusterNodes = <ClusterSizeInNodes>
+
+$storageAccountName_Default = "<DefaultFileSystemStorageAccountName>"
+$containerName_Default = "<DefaultFileSystemContainerName>"
+
+$storageAccountName_Add1 = "<AdditionalStorageAccountName>"
+
+$hiveSQLDatabaseServerName = "<SQLDatabaseServerNameForHiveMetastore>"
+$hiveSQLDatabaseName = "<SQLDatabaseDatabaseNameForHiveMetastore>"
+$oozieSQLDatabaseServerName = "<SQLDatabaseServerNameForOozieMetastore>"
+$oozieSQLDatabaseName = "<SQLDatabaseDatabaseNameForOozieMetastore>"
+
+# Get the virtual network ID and subnet name
+$vnetID = "<AzureVirtualNetworkID>"
+$subNetName = "<AzureVirtualNetworkSubNetName>"
+
+# Get the Storage account keys
+Select-AzureSubscription $subscriptionName
+$storageAccountKey_Default = Get-AzureStorageKey $storageAccountName_Default | %{ $_.Primary }
+$storageAccountKey_Add1 = Get-AzureStorageKey $storageAccountName_Add1 | %{ $_.Primary }
+
+$oozieCreds = Get-Credential -Message "Oozie metastore"
+$hiveCreds = Get-Credential -Message "Hive metastore"
+
+# Create a Blob storage container
+$dest1Context = New-AzureStorageContext -StorageAccountName $storageAccountName_Default -StorageAccountKey $storageAccountKey_Default  
+New-AzureStorageContainer -Name $containerName_Default -Context $dest1Context
+
+# Create a new HDInsight cluster
+$config = New-AzureHDInsightClusterConfig -ClusterSizeInNodes $clusterNodes |
+    Set-AzureHDInsightDefaultStorage -StorageAccountName "$storageAccountName_Default.blob.core.windows.net" -StorageAccountKey $storageAccountKey_Default -StorageContainerName $containerName_Default |
+    Add-AzureHDInsightStorage -StorageAccountName "$storageAccountName_Add1.blob.core.windows.net" -StorageAccountKey $storageAccountKey_Add1 |
+    Add-AzureHDInsightMetastore -SqlAzureServerName "$hiveSQLDatabaseServerName.database.windows.net" -DatabaseName $hiveSQLDatabaseName -Credential $hiveCreds -MetastoreType HiveMetastore |
+    Add-AzureHDInsightMetastore -SqlAzureServerName "$oozieSQLDatabaseServerName.database.windows.net" -DatabaseName $oozieSQLDatabaseName -Credential $oozieCreds -MetastoreType OozieMetastore |
+        New-AzureHDInsightCluster -Name $clusterName -Location $location -VirtualNetworkId $vnetID -SubnetName $subNetName
---
+++ provision 2.ps1
@@ -0,0 +1,158 @@
+﻿# Sign in to Azure
+try 
+{
+    $subscriptiondetails = Get-AzureRmSubscription
+}
+catch
+{
+    Write-Host "Can't get RM subscription information, not authenticated to a subscription" -ForegroundColor Red
+    Login-AzureRmAccount
+}
+finally
+{
+}
+
+
+#variable setup
+$token ="hdinsightsjb"
+$resourceGroupName = $token + "rg"      # Provide a Resource Group name
+$vnetName = $token + "vnet"             #provide virtual network name
+$subnetName = $token + "hdisubnet"      # Provide a virtual network subnet name
+$clusterName = $token
+$defaultStorageAccountName = $token + "store"   # Provide a Storage account name
+$defaultStorageContainerName = $token + "container"
+$location = "Australia Southeast"     # Change the location if needed
+$clusterNodes = 1           # The number of nodes in the HDInsight cluster
+$sqlservername = $token + "dbserver"
+$sqldatabasename = $token + "db"
+
+# Select the subscription to use if you have multiple subscriptions
+#$subscriptionID = "<SubscriptionName>"        # Provide your Subscription Name
+#Select-AzureRmSubscription -SubscriptionId $subscriptionID
+
+# Create an Azure Resource Group
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+# Create an Azure Storage account and container used as the default storage
+New-AzureRmStorageAccount `
+    -ResourceGroupName $resourceGroupName `
+    -StorageAccountName $defaultStorageAccountName `
+    -Location $location `
+    -Type Standard_LRS
+$defaultStorageAccountKey = (Get-AzureRmStorageAccountKey -Name $defaultStorageAccountName -ResourceGroupName $resourceGroupName)[0].Value
+$destContext = New-AzureStorageContext -StorageAccountName $defaultStorageAccountName -StorageAccountKey $defaultStorageAccountKey
+New-AzureStorageContainer -Name $defaultStorageContainerName -Context $destContext
+
+# Create an HDInsight cluster
+$credentials = Get-Credential -Message "Enter Cluster user credentials" -UserName "admin"
+$sshCredentials = Get-Credential -Message "Enter SSH user credentials"
+
+# The location of the HDInsight cluster must be in the same data center as the Storage account.
+$location = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $defaultStorageAccountName | %{$_.Location}
+
+######################################################
+#create network
+# https://azure.microsoft.com/en-us/documentation/articles/hdinsight-extend-hadoop-virtual-network/
+######################################################
+
+New-AzureRmVirtualNetwork -ResourceGroupName $resourceGroupName -Name $vnetName `
+    -AddressPrefix 192.168.0.0/16 -Location $location   
+
+
+
+# Get the Virtual Network object
+$vnet = Get-AzureRmVirtualNetwork `
+    -Name $vnetName `
+    -ResourceGroupName $resourceGroupName
+
+Add-AzureRmVirtualNetworkSubnetConfig -Name $subnetName `
+    -VirtualNetwork $vnet -AddressPrefix 192.168.1.0/24
+
+# Get the region the Virtual network is in.
+$location = $vnet.Location
+# Get the subnet object
+$subnet = $vnet.Subnets | Where-Object Name -eq $subnetName
+# Create a new Network Security Group.
+# And add exemptions for the HDInsight health and management services.
+$nsg = New-AzureRmNetworkSecurityGroup `
+    -Name "hdisecure" `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -name "hdirule1" `
+        -Description "HDI health and management address 168.61.49.99" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "168.61.49.99" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 300 `
+        -Direction Inbound `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -Name "hdirule2" `
+        -Description "HDI health and management 23.99.5.239" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "23.99.5.239" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 301 `
+        -Direction Inbound `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -Name "hdirule3" `
+        -Description "HDI health and management 168.61.48.131" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "168.61.48.131" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 302 `
+        -Direction Inbound `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -Name "hdirule4" `
+        -Description "HDI health and management 138.91.141.162" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "138.91.141.162" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 303 `
+        -Direction Inbound
+# Set the changes to the security group
+Set-AzureRmNetworkSecurityGroup -NetworkSecurityGroup $nsg
+# Apply the NSG to the subnet
+Set-AzureRmVirtualNetworkSubnetConfig `
+    -VirtualNetwork $vnet `
+    -Name $subnetName `
+    -AddressPrefix $subnet.AddressPrefix `
+    -NetworkSecurityGroupId $nsg
+
+
+New-AzureRmHDInsightCluster -ClusterName $clusterName `
+    -ResourceGroupName $resourceGroupName `
+    -HttpCredential $credentials `
+    -Location $location `
+    -DefaultStorageAccountName "$defaultStorageAccountName.blob.core.windows.net" `
+    -DefaultStorageAccountKey $defaultStorageAccountKey `
+    -DefaultStorageContainer $defaultStorageContainerName  `
+    -ClusterSizeInNodes $clusterNodes `
+    -ClusterType Hadoop `
+    -OSType Linux `
+    -Version "3.4" `
+    -SshCredential $sshCredentials `
+    -VirtualNetworkId $vnet.Id `
+    -SubnetName $subnetName 
+    
+
+#########################
+# create SQL database
+#########################
+$sqladmin = Get-Credential
+$sqlserver = New-AzureRmSqlServer -ResourceGroupName $resourceGroupName -ServerName $sqlservername -Location $location -SqlAdministratorCredentials $sqladmin
+$sqldatabase = New-AzureRmSqlDatabase -DatabaseName $sqldatabasename -ServerName $sqlservername -ResourceGroupName $resourceGroupName
+
+
